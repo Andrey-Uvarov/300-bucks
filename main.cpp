5d90e4daf83650b8c2382ee54b801a946d140b5d
Dictator <aouvarov8@gmail.com>
2020-12-11 17:04:15 +0200

    pip

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ void printMatrix(int** matrix, int n, int m);
 void fillMatrix(int** matrix, int n, int m);
 void deleteMatrix(int** matrix, int n, int m);
 void changeRomColumn(int** matrix, int n, int m, int k); //task 11
+int getRowNumWithMaxAvg(int** matrix, int n, int m);
 
 int main() {
 	srand(time(NULL)); 
@@ -22,6 +23,16 @@ int main() {
 	cout << "Matrix after change " << index + 1 << "row and column" << endl;
 	printMatrix(matrix1, size1, size1);
 	deleteMatrix(matrix1, size1, size1);
+
+	//task 10
+	int size2 = 3;
+	int** matrix2 = formMatrix(size2, size2);
+	fillMatrix(matrix2, size2, size2);
+	cout << "Matrix: " << endl;
+	printMatrix(matrix2, size2, size2);
+	int index1 = getRowNumWithMaxAvg(matrix2, size2, size2);
+	cout << "Row index with max average: " << index + 1 << endl;
+
 	return 0;
 }
 
@@ -77,4 +88,23 @@ void changeRomColumn(int** matrix, int n, int m, int k)
 		}
 	}
 	*/
+}
+
+int getRowNumWithMaxAvg(int** matrix, int n, int m)
+{
+
+	int rowIndex = 0;
+	double max = -9999;
+	for (int i = 0; i < n; i++) {
+		int sum = 0;
+		for (int j = 0; j < m; j++) {
+			sum += matrix[i][j];
+		}
+		double avg = sum / m; //avg
+		if (avg > max) {
+			max = avg;
+			rowIndex = i;
+		}
+	}
+	return rowIndex;
 }
